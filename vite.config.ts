01f38b7cb1f46eda1fd56aec5e9ae353dc448469
nooshin bakhtiari <nooshin.bakhtiari96@gmail.com>
2026-06-25 16:10:46 +0330

    feat: add new files to the dashboard project

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
-import react from "@vitejs/plugin-react";
+import { reactRouter } from "@react-router/dev/vite";
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [tailwindcss(), reactRouter()],
+  resolve: {
+    tsconfigPaths: true,
+  },
 });
